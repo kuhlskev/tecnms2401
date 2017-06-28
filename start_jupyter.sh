f32e02e467a7022aff4d37cd20e633a6afe397dc
Kevin Kuhls <kekuhls@cisco.com>
2017-06-28 10:43:47 -0700

    adding jupyter

---
+++ start_jupyter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Prepare environment for DEVNET2449 Lab
+
+#vagrant up csr
+docker run -it --rm -p58889:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
+#then open browser to http://127.0.0.1:58889
