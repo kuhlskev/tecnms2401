ebd463a03694ec0e6e77ebd1275620903b28e167
Kevin Kuhls <kekuhls@cisco.com>
2018-06-09 14:41:30 -0400

    prep for clus18

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,5 +2,5 @@
 # Prepare environment for DEVNET2449 Lab
 
 #vagrant up csr
-docker run -it --rm -p58889:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
+docker run -it --rm -p58887:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
 #then open browser to http://127.0.0.1:58889
