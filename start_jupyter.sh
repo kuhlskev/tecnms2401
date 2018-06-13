570bb7a4b5a4fd7d5e23bddbc7b9d6402ed77a7c
Kevin Kuhls <kekuhls@cisco.com>
2018-06-13 09:43:09 -0400

    Update start_jupyter.sh

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -3,4 +3,4 @@
 
 #vagrant up csr
 docker run -it --rm -p58887:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
-#then open browser to http://127.0.0.1:58889
+#then open browser to http://127.0.0.1:58887
