705e0e5a2087be9ee15a2fd3f1397979a9580f07
Kevin Kuhls <kekuhls@cisco.com>
2018-06-18 11:14:19 -0400

    updating start and stop scripts

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Prepare environment for TECNMS-2401
 
+#Start up csr in vagrant, if applicable
 #vagrant up csr
-docker run -it --rm -p58887:58888 -v$(pwd):/home/docker/ kuhlskev/ansible_ydk_jupyter
+docker run -d -p58887:58888 -v$(pwd):/home/docker/ --name "tecnms2401" kuhlskev/ansible_ydk_jupyter
+#pause for a few seconds to let jupyter startup
+sleep 2
 #then open browser to http://127.0.0.1:58887
 open http://localhost:58887/notebooks/TECNMS-2401.ipynb
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Prepare environment for DEVNET2449 Lab
+
+vagrant suspend csr
+docker rm -f tecnms2401
